0f3aec5aa62a231eb071a148197ba787273673b4
melvil <paradise-interactive@web.de>
2017-06-02 13:26:49 +0200

    loading deck in deck browser

--- app/src/main/java/de/melvil/stacksrs/view/DeckBrowserActivity.java
+++ app/src/main/java/de/melvil/stacksrs/view/DeckBrowserActivity.java
@@ -2,12 +2,29 @@ package de.melvil.stacksrs.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import de.melvil.stacksrs.model.Deck;
 
 public class DeckBrowserActivity extends AppCompatActivity {
+
+    private Deck deck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deck_browser);
+
+        String deckName = getIntent().getStringExtra("deck name");
+        setTitle(deckName);
+        try {
+            deck = Deck.loadDeck(deckName);
+        } catch(IOException e){
+            Toast.makeText(getApplicationContext(), "Deck could not be loaded...",
+                    Toast.LENGTH_SHORT).show();
+            // TODO better error handling
+        }
     }
 }

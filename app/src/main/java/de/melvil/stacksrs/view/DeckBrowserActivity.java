94ce77fa266c59e7c7a3ef9faa72ff69bbcac55f
melvil <paradise-interactive@web.de>
2017-06-02 13:08:08 +0200

    added new activity for deck browser

---
+++ app/src/main/java/de/melvil/stacksrs/view/DeckBrowserActivity.java
@@ -0,0 +1,13 @@
+package de.melvil.stacksrs.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeckBrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deck_browser);
+    }
+}
